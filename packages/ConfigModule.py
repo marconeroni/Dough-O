733e5a54250d9bd651af7f5d2ba349ed3e83e538
marconeroni <dragonfit@gmail.com>
2021-02-15 21:35:38 +0100

    commit

--- packages/ConfigModule.py
+++ packages/ConfigModule.py
@@ -558,7 +558,7 @@ class ConfigModule(object):
 
     @classmethod
     def launch_browser(cls):
-        App.get_running_app().root_window.height = 400
+        App.get_running_app().root_window.size = (800,400)
         #try:
             #cmd = 'chromium-browser --start-maximized'
             #sb = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell = True, universal_newlines=True)
